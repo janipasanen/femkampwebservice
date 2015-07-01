183ba856c70580bbc3ca029237291c24e0c590cf
Jani Pasanen <jani.m.pasanen@gmail.com>
2015-07-02 00:27:50 +0200

    updates

---
+++ FemkampApp/Controllers/FemkampController.cs
@@ -0,0 +1,35 @@
+﻿using FemkampApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FemkampApp.Controllers
+{
+    public class FemkampController : ApiController
+    {
+        Femkamp[] femkamp = new Femkamp[]
+        {
+            new Femkamp { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Femkamp { Id = 2, Name = "Yo-yo", Category = "Groceries", Price = 3.75M },
+            new Femkamp { Id = 3, Name = "Hammer", Category = "Groceries", Price = 16.99M },
+        };
+
+        public IEnumerable<Femkamp> GetAllProducts()
+        {
+            return femkamp;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var femkamp = femkamp.FirstOrDefault((p) => p.Id == id);
+            if (femkamp == null)
+            {
+                return NotFound();
+            }
+            return Ok(femkamp);
+        }
+    }
+}
